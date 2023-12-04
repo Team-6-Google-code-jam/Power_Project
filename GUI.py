e5fb6bfb56f9a0ac4a250574f6c16e8c94646411
Alex <alex.burnett7@gmail.com>
2023-12-04 23:13:25 +0000

    added a home page

--- GUI.py
+++ GUI.py
@@ -11,12 +11,25 @@ from matplotlib.figure import Figure
 class Application(ttk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
         self.pack(fill="both", expand=True, side=TOP)
-        
         style = Style("superhero")
-        # style.configure('TLabel', font=('Helvetica', 24))
+        style.configure('TLabel', font=('Helvetica', 24))
         
+        self.manual = ttk.Frame(self)
+        self.manual.grid(row=0,column=0,sticky="nsew")
+        self.manual_label = ttk.Label(self.manual,text='Enter price Per Kwh manually:')
+        self.manual_input = ttk.Entry(self.manual)
+        self.manual_Button = ttk.Button(self.manual,text = 'Submit',command=self.manual_push_cost)
+        self.manual_label.grid(row=0)
+        self.manual_Button.grid(row=2)
+        self.manual_input.grid(row=1)
+    def update(self):
+        value = r.randint(0,200)
+        self.Power_Monitor_meter.configure(amountused=value)
+        self.Cost.configure(text=f'£{round(float(value * self.Rate),2)}')
+        self.Power_Monitor_meter.after(200,self.update)
+        
+    def mainpage(self):
         self.Cost_Display = ttk.Frame(self)
         self.Cost_Display.grid(row= 1, column=0, sticky="nsew")
         self.Cost_header = ttk.Label(self.Cost_Display,text="Cost:")
@@ -30,87 +43,24 @@ class Application(ttk.Frame):
         self.Power_Monitor_meter = ttk.Meter(self.Power_Monitor, bootstyle="info", textright="", subtext="Kw/h", amountused=0, interactive=False,metertype='semi',amounttotal=1000)
         self.Power_Monitor_meter.pack(fill="both", expand=True, side=TOP, pady=30, padx=30)
         
-        self.graph_display = ttk.Frame(self)
-        self.graph_display.grid(row= 1, column=1, sticky="nsew")
-        self.graph_header = ttk.Label(self.graph_display,text="Cost:")
-        self.graph_header.pack()
-        self.graph = Figure(figsize = (5,4), dpi = 100)
-        self.ax = self.graph.add_subplot()
-        t = np.arange(0, 3, .01)
-        self.line, = self.ax.plot(t, 2 * np.sin(2 * np.pi * t))
-        self.ax.set_xlabel("time [s]")
-        self.ax.set_ylabel("f(t)")
-        self.canvas = FigureCanvasTkAgg(self.graph, master=self.graph_display)
-        self.canvas.draw()
         self.Power_Monitor_meter.after(1000,self.update)
-    def update(self):
-        value = r.randint(0,200)
-        self.Power_Monitor_meter.configure(amountused=value)
-        self.Cost.configure(text=f'£{round(float(value * 0.27),2)}')
-        self.Power_Monitor_meter.after(200,self.update)
+    
+    def manual_push_cost(self):
+        self.Rate = float(self.manual_input.get())
+        self.wipe()
         
-
+    
+    def wipe(self):
+        windows = self.winfo_children()
+        print(windows)
+        for item in windows:
+            if item.winfo_children():
+                windows.extend(item.winfo_children())
+        for item in windows:
+            print('passed')
+            item.destroy()
+        self.mainpage()
 if __name__ == "__main__":
     app = ttk.Window("Power Monitor", themename="superhero")
     Application(app)
     app.mainloop()
-
-# import tkinter
-
-# import numpy as np
-
-# # Implement the default Matplotlib key bindings.
-# from matplotlib.backend_bases import key_press_handler
-# from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
-#                                                NavigationToolbar2Tk)
-# from matplotlib.figure import Figure
-
-# root = tkinter.Tk()
-# root.wm_title("Embedding in Tk")
-
-# fig = Figure(figsize=(5, 4), dpi=100)
-# t = np.arange(0, 3, .01)
-# ax = fig.add_subplot()
-# line, = ax.plot(t, 2 * np.sin(2 * np.pi * t))
-# ax.set_xlabel("time [s]")
-# ax.set_ylabel("f(t)")
-
-# canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-# canvas.draw()
-
-# # pack_toolbar=False will make it easier to use a layout manager later on.
-# toolbar = NavigationToolbar2Tk(canvas, root, pack_toolbar=False)
-# toolbar.update()
-
-# canvas.mpl_connect(
-#     "key_press_event", lambda event: print(f"you pressed {event.key}"))
-# canvas.mpl_connect("key_press_event", key_press_handler)
-
-# button_quit = tkinter.Button(master=root, text="Quit", command=root.destroy)
-
-
-# def update_frequency(new_val):
-#     # retrieve frequency
-#     f = float(new_val)
-
-#     # update data
-#     y = 2 * np.sin(2 * np.pi * f * t)
-#     line.set_data(t, y)
-
-#     # required to update canvas and attached toolbar!
-#     canvas.draw()
-
-
-# slider_update = tkinter.Scale(root, from_=1, to=5, orient=tkinter.HORIZONTAL,
-#                               command=update_frequency, label="Frequency [Hz]")
-
-# # Packing order is important. Widgets are processed sequentially and if there
-# # is no space left, because the window is too small, they are not displayed.
-# # The canvas is rather flexible in its size, so we pack it last which makes
-# # sure the UI controls are displayed as long as possible.
-# button_quit.pack(side=tkinter.BOTTOM)
-# slider_update.pack(side=tkinter.BOTTOM)
-# toolbar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
-# canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
-
-# tkinter.mainloop()
