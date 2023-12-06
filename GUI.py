c794f3f018a4efbca91d9eb1b5cf58dd5c12dfeb
Alex <alex.burnett7@gmail.com>
2023-12-06 20:54:34 +0000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -15,7 +15,9 @@ class Application(ttk.Frame):
         '''
         self.timeconversion = 1/60/60/5
         self.time = 0
-        self.average_cache = deque(maxlen=1000)
+        self.average_cache = deque(
+            maxlen=1000
+            )
         super().__init__(
             *args,
             **kwargs
@@ -32,31 +34,96 @@ class Application(ttk.Frame):
         self.load_settings()
         
     def update(self):
+        '''
+        Run An update cycle
+        '''
         self.time += self.timeconversion
-        self.current_draw = float(cpu_percent() * self.cpu_ratio)
-        self.average_cache.append(self.current_draw)
+        self.current_draw = float(
+            cpu_percent() * self.cpu_ratio
+            )
+        self.average_cache.append(
+            self.current_draw
+            )
         if self.total_use == 0:
             self.total_use = self.time * 250
         else:
             self.total_use += self.current_draw * self.timeconversion
-        self.price = self.total_use * self.Rate * 0.001
-        print(self.price)
-        self.Cost.configure(text=f'£{round(self.price,2)}')
-        self.time_monitor_meter.configure(amountused=round(self.time,2))
-        self.power_monitor_meter.configure(amountused=round(self.current_draw,2))
-        self.average.configure(text = f'{round(np.mean(self.average_cache),2)}w/h')
-        self.total.configure(text=f'{round(self.total_use,2)}w')
-        self.Cost.after(200,self.update)
+        self.price = self.total_use * self.Rate * 0.01
+        self.Cost.configure(
+            text=f'£{round(self.price,2)}')
+        self.time_monitor_meter.configure(
+            amountused=round(
+                self.time,
+                2
+                )
+            )
+        self.power_monitor_meter.configure(
+            amountused=round(
+                self.current_draw,
+                2)
+            )
+        self.average.configure(
+            text = f'{round(np.mean(self.average_cache),2)}w/h'
+                    )
+        self.total.configure(
+            text=f'{round(self.total_use,2)}w'
+                )
+        self.Cost.after(
+            200,
+            self.update
+            )
     def mainpage(self):
-        style = Style(self.Mode)
-        self.Cost_Display = ttk.Frame(self,style='dark')
-        self.Cost_Display.grid(row= 1, column=1,rowspan=2, sticky="nsew")
-        self.Cost_header = ttk.Label(self.Cost_Display,text="Estimated cost for this session:\n",font=('Helvetica', 24),bootstyle="inverse-dark")
-        self.Cost = ttk.Label(self.Cost_Display,text="£0.00",font=('Helvetica', 24),bootstyle="inverse-dark")
-        self.average_header = ttk.Label(self.Cost_Display,text="\n\nAverage system power draw:\n",font=('Helvetica', 24),bootstyle="inverse-dark")
-        self.average = ttk.Label(self.Cost_Display,text="135.06w",font=('Helvetica', 24),bootstyle="inverse-dark")
-        self.total_header = ttk.Label(self.Cost_Display,text="\n\nTotal power use this session:\n",font=('Helvetica', 24),bootstyle="inverse-dark")
-        self.total = ttk.Label(self.Cost_Display,text=f'{self.total_use}w',font=('Helvetica', 24),bootstyle="inverse-dark")
+        '''
+        Generates the Main page
+        '''
+        style = Style(
+            self.Mode
+            )
+        self.Cost_Display = ttk.Frame(
+            self,style='dark'
+            )
+        self.Cost_Display.grid(
+            row= 1,
+            column=1,
+            rowspan=2, 
+            sticky="nsew"
+            )
+        self.Cost_header = ttk.Label(
+            self.Cost_Display,
+            text="Estimated cost for this session:\n",
+            font=('Helvetica', 24),
+            bootstyle="inverse-dark"
+            )
+        self.Cost = ttk.Label(
+            self.Cost_Display,
+            text="£0.00",
+            font=('Helvetica', 24),
+            bootstyle="inverse-dark"
+            )
+        self.average_header = ttk.Label(
+            self.Cost_Display,
+            text="\n\nAverage system power draw:\n",
+            font=('Helvetica', 24),
+            bootstyle="inverse-dark"
+            )
+        self.average = ttk.Label(
+            self.Cost_Display,
+            text="135.06w",
+            font=('Helvetica', 24),
+            bootstyle="inverse-dark"
+            )
+        self.total_header = ttk.Label(
+            self.Cost_Display,
+            text="\n\nTotal power use this session:\n",
+            font=('Helvetica', 24),
+            bootstyle="inverse-dark"
+            )
+        self.total = ttk.Label(
+            self.Cost_Display,
+            text=f'{self.total_use}w',
+            font=('Helvetica', 24),
+            bootstyle="inverse-dark"
+            )
         self.Cost_header.pack()
         self.Cost.pack()
         self.total_header.pack()
@@ -64,33 +131,119 @@ class Application(ttk.Frame):
         self.average_header.pack()
         self.average.pack()
         
-        self.time_monitor = ttk.Frame(self,style="default")
-        self.time_monitor.grid(row=1, column=0, sticky="nsew")
-        self.time_monitor_header = ttk.Label(self.time_monitor,text='Uptime this Session:')
-        self.time_monitor_meter = ttk.Meter(self.time_monitor, bootstyle="info", textright="", subtext="Hours", amountused=0, interactive=False,amounttotal=24,stripethickness=10)
+        self.time_monitor = ttk.Frame(
+            self,
+            style="default"
+            )
+        self.time_monitor.grid(
+            row=1,
+            column=0,
+            sticky="nsew"
+            )
+        self.time_monitor_header = ttk.Label(
+            self.time_monitor,
+            text='Uptime this Session:'
+            )
+        self.time_monitor_meter = ttk.Meter(
+            self.time_monitor,
+            bootstyle="info",
+            textright="",
+            subtext="Hours",
+            amountused=0,
+            interactive=False,
+            amounttotal=24,
+            stripethickness=10
+            )
         self.time_monitor_header.pack()
-        self.time_monitor_meter.pack(fill="both", expand=True, side=TOP, pady=30, padx=30)
+        self.time_monitor_meter.pack(
+            fill="both",
+            expand=True,
+            side=TOP,
+            pady=30,
+            padx=30)
         
-        self.power_monitor = ttk.Frame(self,style='default')
-        self.power_monitor.grid(row=2, column=0, sticky="nsew")
-        self.power_monitor_header = ttk.Label(self.power_monitor,text='Power Usage:')
-        self.power_monitor_meter = ttk.Meter(self.power_monitor, bootstyle="info", textright="", subtext="W/h", amountused=0,metertype='semi', interactive=False,amounttotal=1000,subtextstyle="primary")
+        self.power_monitor = ttk.Frame(
+            self,
+            style='default'
+            )
+        self.power_monitor.grid(
+            row=2,
+            column=0,
+            sticky="nsew"
+            )
+        self.power_monitor_header = ttk.Label(
+            self.power_monitor,
+            text='Power Usage:'
+            )
+        self.power_monitor_meter = ttk.Meter(
+            self.power_monitor,
+            bootstyle="info", 
+            textright="", 
+            subtext="W/h", 
+            amountused=0,
+            metertype='semi',
+            interactive=False,
+            amounttotal=1000,
+            subtextstyle="primary"
+            )
         self.power_monitor_header.pack()
-        self.power_monitor_meter.pack(fill="both", expand=True, side=TOP, pady=30, padx=30)
+        self.power_monitor_meter.pack(
+            fill="both",
+            expand=True,
+            side=TOP,
+            pady=30,
+            padx=30
+            )
         
-        self.settings = ttk.Frame(self,style="secondary")
-        self.settings.grid(column=0,row=0,columnspan=2, sticky="nsew")
-        self.settings_button  = ttk.Button(self.settings,text='Settings')
-        self.settings_button.pack(side=LEFT)
+        self.settings = ttk.Frame(
+            self,
+            style="secondary"
+            )
+        self.settings.grid(
+            column=0,
+            row=0,
+            columnspan=2, 
+            sticky="nsew"
+            )
+        self.settings_button  = ttk.Button(
+            self.settings,
+            text='Settings',
+            command=self.tosettings
+            )
+        self.settings_button.pack(
+            side=LEFT
+            )
         
         self.after(100,self.update)
-    #TODO
-    # def settingspage(self):
-    #     self.settings = ttk.Frame(self,style="secondary")
-    #     self.settings.grid(column=0,row=0,columnspan=2, sticky="nsew")
-    #     self.home_button  = ttk.Button(self.settings,text='Home',command=self.wipe)
-    #     self.settings_button  = ttk.Button(self.settings,text='Home',command=self.wipe)
-    #     self.settings_button.pack(side=LEFT)
+    def settingspage(self):
+        self.settings = ttk.Frame(
+            self,
+            style="secondary"
+            )
+        self.settings.grid(
+            column=0,
+            row=0,
+            columnspan=2,
+            sticky="nsew"
+            )
+        self.home_button  = ttk.Button(
+            self.settings,
+            text='Home',
+            command=self.tomain,
+            style='danger'
+            )
+        self.settings_button  = ttk.Button(
+            self.settings,
+            text='Save',
+            style='success'
+            )
+        self.home_button.pack(
+            side=LEFT
+            )
+        self.settings_button.pack(
+            side=LEFT
+            )
+        
     
     def load_settings(self):
         '''
@@ -209,6 +362,9 @@ class Application(ttk.Frame):
         self.wipe()
         self.mainpage()
 
+    def tosettings(self):
+        self.wipe()
+        self.settingspage()
 if __name__ == "__main__":
     app = ttk.Window("Power Monitor")
     Application(app)
