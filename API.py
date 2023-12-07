f1591533d3a468c664186f3b287241ba770ebe5b
tvtn24 <122183965+tvtn24@users.noreply.github.com>
2023-12-07 16:30:02 +0000

    Update API.py

--- API.py
+++ API.py
@@ -1,12 +1,13 @@
-import json
 import requests
 from datetime import datetime
 
-def get_electricity_prices(dno):
+def get_electricity_price(dno):
     url = "https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices"
-
+    
+  
     current_datetime = datetime.now()
     
+    # Format the date and time as needed
     start_date = current_datetime.strftime("%d-%m-%Y")
     end_date = current_datetime.strftime("%d-%m-%Y")
 
@@ -21,8 +22,8 @@ def get_electricity_prices(dno):
 
     if response.status_code == 200:
         data = response.json()  
-        return data
+        overall_price = data.get('data', {}).get('data', [{}])[0].get('Overall')
+        return overall_price
     else:
         print("Failed to fetch data. Status code:", response.status_code)
         return None
-
