e5c56fc615e59c92affe544c922612b3c59e406e
Alex <alex.burnett7@gmail.com>
2023-12-07 14:04:37 +0000

    adding the ability for power predictor to activate on launch

--- Startup_setup.py
+++ Startup_setup.py
@@ -1,14 +1,3 @@
-import getpass
-import os
-USER_NAME = getpass.getuser()
+from power_predictor import add_to_startup
 
-
-def add_to_startup(file_path="",file=__file__):
-    if file_path == "":
-        file_path = os.path.dirname(os.path.realpath(file))
-    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
-    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
-        bat_file.write(f'python "" "{file_path}\{file}"')
-
-if __name__ == "__main__":
-    add_to_startup(file='power_predictor.py')
+add_to_startup()
--- power_predictor.py
+++ power_predictor.py
@@ -3,6 +3,11 @@ import time
 
 start_time = None
 end_time = None
+import getpass
+import os
+USER_NAME = getpass.getuser()
+
+
 
 # Sample values for the idle and max power (just for reference)
 # idle_power = 20
@@ -20,6 +25,19 @@ def initialise():
     start_time = time.time()
     with open('timestamp.txt', 'w') as f:
         f.write(str(start_time))
+
+def add_to_startup(file_path=""):
+    if file_path == "":
+        file_path = os.path.dirname(
+            os.path.realpath(__file__)
+            )
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    with open(
+        bat_path + '\\' + "open.bat", "w+"
+        ) as bat_file:
+        bat_file.write(
+            r'start "" "%s"' % file_path
+            )
 
 if __name__ == '__main__':
     initialise()
